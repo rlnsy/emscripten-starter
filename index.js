100ec1044728057a852629b1f91fef958e4e58ba
rlnsy <rowlindsay@gmail.com>
2020-12-12 19:27:34 -0800

    add logging to loader

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import * as a from "./a.out.js";
 
-export const add = () => {
-	a.ccall("add", 'number', ['number', 'number'], [2,2]);
-}
+const result = await a()
+		.then((result) => result)
+		.catch((err) => {
+			console.log("error loading wasm module");
+			console.log(err);
+		});
+
+console.log(result);
+
+//export const add = () => {
+	//a.ccall("add", 'number', ['number', 'number'], [2,2]);
+//}
