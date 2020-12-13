9a72fe34c1d0d4913cc0ea86e19caf020e3c8f3b
rlnsy <rowlindsay@gmail.com>
2020-12-12 20:19:07 -0800

    use single file mode for web binary

--- Makefile
+++ Makefile
@@ -2,10 +2,11 @@ CC=emcc
 
 FUNS="_add"
 FLGS=-s MODULARIZE -s EXPORTED_FUNCTIONS='[$(FUNS)]' -s EXPORTED_RUNTIME_METHODS='["ccall"]'
+WEB_FLAGS=-s ENVIRONMENT='web' -s SINGLE_FILE=1
 node: hello.c
 	$(CC) $(FLGS) hello.c
 web: hello.c
-	$(CC) $(FLGS) -s ENVIRONMENT='web' hello.c
+	$(CC) $(FLGS) $(WEB_FLAGS) hello.c
 clean:
 	rm ./a.out.js ./a.out.wasm
 all: node
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import * as a from "./a.out.js";
 
-a().then((result) => {
-			console.log(result);
-		})
-		.catch((err) => {
-			console.log("error loading wasm module");
-			console.log(err);
-		});
+//a().then((result) => {
+			//console.log(result);
+		//})
+		//.catch((err) => {
+			//console.log("error loading wasm module");
+			//console.log(err);
+		//});
 
-//export const add = () => {
-	//a.ccall("add", 'number', ['number', 'number'], [2,2]);
-//}
+export const add = () => {
+	a.ccall("add", 'number', ['number', 'number'], [2,2]);
+}
