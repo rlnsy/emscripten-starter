38f022ffd06e852bb1b326baa4e6300d95660483
rlnsy <rowlindsay@gmail.com>
2020-12-12 18:12:38 -0800

    simplify and modularize index

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 import * as a from "./a.out.js";
 
-const result = a.ccall("add", 'number', ['number', 'number'], [2,2]);
-
-//window.displayResult = function() {
-	//console.log(result);
-//};
-
-$(function(){
-    $('button').on('click',function(){
-        console.log(result);
-    });
-});
+export const add = () => {
+	a.ccall("add", 'number', ['number', 'number'], [2,2]);
+}
